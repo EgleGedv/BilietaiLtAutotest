dced0960da56d10cefe944715cb63d2732e14ec8
egleg <egle.gdv@gmail.com>
2023-03-09 15:08:38 +0200

    Added new classes Driver and TestListener

---
+++ src/test/java/lt/egle/bilietailt/utils/Driver.java
@@ -0,0 +1,34 @@
+package lt.egle.bilietailt.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Driver {
+
+    public static ThreadLocal<WebDriver> driverThread = new ThreadLocal<>();
+
+    public static void setDriver() {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        chromeOptions.addArguments("--start-maximized");
+        chromeOptions.addArguments("--force-device-scale-factor=0.65");
+        driverThread.set(new ChromeDriver(chromeOptions));
+        driverThread.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    public static WebDriver getDriver() {
+        return driverThread.get();
+    }
+
+    public static void closeDriver() {
+        driverThread.get().quit();
+        driverThread.remove();
+    }
+}
